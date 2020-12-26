8fadbe82d00d9af2f4238d4cd5c8460bf3a86e8b
Frederick PORT <developpeur@stud-ios.fr>
2020-12-26 18:57:20 +0100

    Initial Commit

---
+++ Shared/lazyApp.swift
@@ -0,0 +1,17 @@
+//
+//  lazyApp.swift
+//  Shared
+//
+//  Created by Frederick Port on 26/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct lazyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
